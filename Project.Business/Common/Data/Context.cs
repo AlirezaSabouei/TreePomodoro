8ccb043eff2b7ff973212982e1b31229e998dc18
Alireza Sabouei <alireza.sabouei@gmail.com>
2025-10-13 12:38:42 +0400

    Initial commit

--- Project.Business/Common/Data/Context.cs
+++ Project.Business/Common/Data/Context.cs
@@ -7,7 +7,7 @@ using Project.Domain.Entities;
 
 namespace Project.Business.Common.Data;
 
-public class Context(DbContextOptions<Context> options) : IdentityDbContext(options), IContext
+public class Context(DbContextOptions<Context> options) : IdentityDbContext(options)
 {
     // Example table
     public DbSet<Student> Students { get; set; }
